3a3f205200311352087e1b4f987c6bb9d8e3ac5b
Henry Bautista <henry.bautista.prado@icloud.com>
2023-02-27 21:43:54 -0600

    header con sass

--- market-free/src/Components/Header/Header.jsx
+++ market-free/src/Components/Header/Header.jsx
@@ -6,19 +6,19 @@ const Header = () => {
       <img className="header__logo" src="https://icon-library.com/images/supermarket-icon-png/supermarket-icon-png-28.jpg"></img>
        <ul className="header__nav-list">
         <li className="header__list-item">
-         <a href="/" className="header__item-link header_item-link--is-active">Home</a>   
+         <a href="http://localhost:5173/" className="header__item-link header_item-link--is-active">Home</a>   
         </li>
         <li className="header__list-item">
-        <a href="/" className="header__item-link ">Dashboard</a>
+        <a href="/Dashboard" className="header__item-link ">Dashboard</a>
         </li>
         <li className="header__list-item">
-         <a href="/" className="header__item-link">Secret</a>   
+         <a href="/Secret" className="header__item-link">Secret</a>   
         </li>
         <li className="header__list-item">
-          <a href="/" className="header__item-link">Login</a>
+          <a href="/login" className="header__item-link">Login</a>
         </li>
         <li className="header__list-item">
-          <a href="/" className="header__item-link">Sing Up</a>
+          <a href="/Singup" className="header__item-link">Sing Up</a>
         </li>
        </ul>
     </nav>
