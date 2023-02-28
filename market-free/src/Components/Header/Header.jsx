dbf3d097c7c4ad63aaaedac9ccdcbf9060ad90d4
Henry Bautista <henry.bautista.prado@icloud.com>
2023-02-27 19:40:02 -0600

    Header con BEM

--- market-free/src/Components/Header/Header.jsx
+++ market-free/src/Components/Header/Header.jsx
@@ -1,6 +1,27 @@
+import "./Header.scss"
+
 const Header = () => {
   return (
-    <div>Header</div>
+    <nav className="header">
+      <img className="header__logo" src="https://icon-library.com/images/supermarket-icon-png/supermarket-icon-png-28.jpg"></img>
+       <ul className="header__nav-list">
+        <li className="header__list-item">
+         <a href="/" className="header__item-link header_item-link--is-active">Home</a>   
+        </li>
+        <li className="header__list-item">
+        <a href="/" className="header__item-link ">Dashboard</a>
+        </li>
+        <li className="header__list-item">
+         <a href="/" className="header__item-link">Secret</a>   
+        </li>
+        <li className="header__list-item">
+          <a href="/" className="header__item-link">Login</a>
+        </li>
+        <li className="header__list-item">
+          <a href="/" className="header__item-link">Sing Up</a>
+        </li>
+       </ul>
+    </nav>
   )
 }
 
