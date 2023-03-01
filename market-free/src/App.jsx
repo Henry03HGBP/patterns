8bf54e669f0aaf0a69f3e7a4f90d7ccb85cbb336
Henry Bautista <henry.bautista.prado@icloud.com>
2023-02-28 22:12:50 -0600

    listo

--- market-free/src/App.jsx
+++ market-free/src/App.jsx
@@ -1,10 +1,9 @@
 import './App.css'
 import Header from './Components/Header/Header'
 
-function App() {
-  return(
-    <Header/>
+function App () {
+  return (
+    <Header />
   )
 }
-
 export default App
--- market-free/src/Components/Header/Header.jsx
+++ market-free/src/Components/Header/Header.jsx
@@ -1,27 +1,26 @@
-import "./Header.scss"
-
+import './Header.scss'
 
 const Header = () => {
   return (
-    <nav className="header">
-      <img className="header__logo" src="https://icon-library.com/images/supermarket-icon-png/supermarket-icon-png-28.jpg"></img>
-       <ul className="header__nav-list">
-        <li className="header__list-item">
-         <a href="http://localhost:5173/" className="header__item-link header_item-link--is-active">Home</a>   
+    <nav className='header'>
+      <img className='header__logo' src='https://icon-library.com/images/supermarket-icon-png/supermarket-icon-png-28.jpg' />
+      <ul className='header__nav-list'>
+        <li className='header__list-item'>
+          <a href='http://localhost:5173/' className='header__item-link header_item-link--is-active'>Home</a>
         </li>
-        <li className="header__list-item">
-        <a href="/Dashboard" className="header__item-link ">Dashboard</a>
+        <li className='header__list-item'>
+          <a href='/Dashboard' className='header__item-link '>Dashboard</a>
         </li>
-        <li className="header__list-item">
-         <a href="/Secret" className="header__item-link">Secret</a>   
+        <li className='header__list-item'>
+          <a href='/Secret' className='header__item-link'>Secret</a>
         </li>
-        <li className="header__list-item">
-          <a href="/login" className="header__item-link">Login</a>
+        <li className='header__list-item'>
+          <a href='/login' className='header__item-link'>Login</a>
         </li>
-        <li className="header__list-item">
-          <a href="/Singup" className="header__item-link">Sing Up</a>
+        <li className='header__list-item'>
+          <a href='/Singup' className='header__item-link'>Sing Up</a>
         </li>
-       </ul>
+      </ul>
     </nav>
   )
 }
