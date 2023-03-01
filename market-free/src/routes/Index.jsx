2126e557b33fa286f8e4dbab4f4428238921a035
Henry Bautista <henry.bautista.prado@icloud.com>
2023-02-28 22:12:01 -0600

    no tuvo cambios porque no salio lo del tutorial

--- market-free/src/routes/Index.jsx
+++ market-free/src/routes/Index.jsx
@@ -1,23 +1,23 @@
 import React from 'react'
-import {Routes, Route} from "react-router-dom"
+import { Routes, Route } from 'react-router-dom'
 import { 
-    Home, 
-Dashboard,
-Login,
-Secret,
-Singup
+    Home,
+    Dashboard,
+    Login,
+    Secret,
+    Singup
 } from "@/pages"
 
-const RouterIndex = () => {
-    return(
+const Index = () => {
+  return (
     <Routes>
-        <Route path='/Home' element={<Home/>}/>
+        <Route path='/' element={<Home/>} />
         <Route path='/dashboard' element={<Dashboard/>}/>
-        <Route path='/login' element={<Login/>}/>
+        <Route path='/Login' element={<Login/>}/>
         <Route path='/Secret' element={<Secret/>}/>
         <Route path='/Singup' element={<Singup/>}/>
     </Routes>
-    )
+  )
 }
 
-export default RouterIndex
+export default Index
